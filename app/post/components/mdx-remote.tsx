1cdf59437259dbe1b99f8b48719a62050fe86ba0
leoliq <hualanice@gmail.com>
2024-02-22 21:07:09 +0800

    update

--- app/post/components/mdx-remote.tsx
+++ app/post/components/mdx-remote.tsx
@@ -1,48 +1,53 @@
-import { MDXRemote } from 'next-mdx-remote/rsc'
+import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
 import rehypePrettyCode from "rehype-pretty-code";
 
 const components = {
-  h1: (props) => (
+  a: (props: { children: React.ReactNode }) => (
+    <a {...props} className="text-blue-500">
+      {props.children}
+    </a>
+  ),
+  h1: (props: { children: React.ReactNode }) => (
     <h1 {...props} className="font-bold my-4 text-4xl">
       {props.children}
     </h1>
   ),
-  h2: (props) => (
+  h2: (props: { children: React.ReactNode }) => (
     <h2 {...props} className="font-bold my-4 text-3xl">
       {props.children}
     </h2>
   ),
-  h3: (props) => (
+  h3: (props: { children: React.ReactNode }) => (
     <h3 {...props} className="font-bold my-4 text-2xl">
       {props.children}
     </h3>
   ),
-  h4: (props) => (
+  h4: (props: { children: React.ReactNode }) => (
     <h4 {...props} className="font-bold my-4 text-xl">
       {props.children}
     </h4>
   ),
-  p: (props) => (
+  p: (props: { children: React.ReactNode }) => (
     <p {...props} className="my-4">
       {props.children}
     </p>
   ),
-  ol: (props) => (
+  ol: (props: { children: React.ReactNode }) => (
     <ol {...props} className="my-4">
       {props.children}
     </ol>
   ),
-  ul: (props) => (
+  ul: (props: { children: React.ReactNode }) => (
     <ul {...props} className="my-4">
       {props.children}
     </ul>
   ),
-  pre: (props) => (
+  pre: (props: { children: React.ReactNode }) => (
     <pre {...props} className="my-4 overflow-auto">
       {props.children}
     </pre>
   ),
-  code: (props) => (
+  code: (props: { children: React.ReactNode }) => (
     <code {...props} className="my-4 [&>span]:mx-4">
       {props.children}
     </code>
@@ -50,7 +55,7 @@ const components = {
 
 }
 
-export function CustomMDX(props) {
+export function CustomMDX(props: MDXRemoteProps) {
   return (
     <MDXRemote
       {...props}
